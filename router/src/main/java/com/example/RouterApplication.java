c66329c8102a5e9201ddbcb1543d47e3974f6975
huanghao111 <562556768@qq.com>
2019-10-25 15:50:16 +0800

    zhushi

--- router/src/main/java/com/example/RouterApplication.java
+++ router/src/main/java/com/example/RouterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy //表明该服务具有路由功能
 @SpringBootApplication
 public class RouterApplication {
+    //注解
     public static void main(String[] args) {
         SpringApplication.run(RouterApplication.class,args);
     }
